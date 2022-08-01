7571cd2886e2f40f744a662a194be7a062a3b742
adissontejo <adissonmprtejo@gmail.com>
2022-08-01 05:13:03 -0300

    Fix vercel deploy bug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['pt-BR', 'en', 'es'],
     defaultLocale: 'pt-BR',
+    localePath: path.resolve('./public/locales'),
   },
 };
